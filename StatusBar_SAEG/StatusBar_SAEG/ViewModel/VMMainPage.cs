75558491e6aaa9bf68bec43fecd7a5a7d3ba24dc
Shino2302 <encinas.sigfrido.cbtis37@gmail.com>
2023-10-26 23:03:33 -0700

    Agregar archivos de proyecto.

---
+++ StatusBar_SAEG/StatusBar_SAEG/ViewModel/VMMainPage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace StatusBar_SAEG.ViewModel
+{
+    public class VMMainPage
+    {
+        public VMMainPage()
+        {
+            
+        }
+        public void Ocultar()
+        {
+            DependencyService.Get<VMStatusBar>().OcultarStatusBar();
+        }
+        public void Mostrar()
+        {
+            DependencyService.Get<VMStatusBar>().MostrarStatusBar();
+        }
+        public void Traslicido()
+        {
+            DependencyService.Get<VMStatusBar>().Traslucido();
+        }
+        public void Transparente()
+        {
+            DependencyService.Get<VMStatusBar>().Transparente();
+        }
+        public void CambiarColor()
+        {
+            DependencyService.Get<VMStatusBar>().CambiarColor();
+        }
+
+        public ICommand OcuultarCommand => new Command(Ocultar);
+        public ICommand MostrarCommand => new Command(Mostrar);
+        public ICommand TraslucidoCommand => new Command(Traslicido);
+        public ICommand TransparenteCommand => new Command(Transparente);
+        public ICommand CambiarColorCommand => new Command(CambiarColor);
+
+    }
+}
---
+++ StatusBar_SAEG/StatusBar_SAEG/ViewModel/VMStatusBar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatusBar_SAEG.ViewModel
+{
+    public interface VMStatusBar
+    {
+        void OcultarStatusBar();
+        void MostrarStatusBar();
+        void Traslucido();
+        void Transparente();
+        void CambiarColor();
+    }
+}
